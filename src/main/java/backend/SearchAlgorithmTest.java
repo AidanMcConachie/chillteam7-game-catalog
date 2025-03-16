8b89744ff254b9b47e9e2c7be18a6bda32c5df38
Alexis-jg-ryan <alexisjgryan@gmail.com>
2025-03-16 16:20:22 -0400

    finished my work for iteration 2

---
+++ src/main/java/backend/SearchAlgorithmTest.java
@@ -0,0 +1,29 @@
+package backend;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SearchAlgorithmTest {
+    private List<Card> testCards;
+
+    @Test
+    public void testAlgorithm() {
+        String search = "F";
+        testCards = new ArrayList<>();
+        Card finalFantasyCard = new Card("Final Fantasy", new String[]{"RPG"}, "5", "Fantasy role-playing game", "ff.jpg");
+        testCards.add(finalFantasyCard);
+        List<Card> results = searchGames(search);
+
+        // Initialize the original list in SortGame
+        assertEquals(testCards, results);
+    }
+
+    public static List<Card> searchGames(String search) {
+        return new ArrayList<>();
+    }
+}
